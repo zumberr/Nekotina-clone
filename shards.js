6450881145e03a50a6a81a5f79a9ff55b8adb967
zumber <84306702+zumberr@users.noreply.github.com>
2024-02-01 22:31:21 -0500

    Update shards.js

--- shards.js
+++ shards.js
@@ -1,5 +1,5 @@
 const {ShardingManager} = require("discord.js");
-const config = require("./inhibitors/filter.json")["Configuracion General"].token;
+const config = require("./source/inhibitors/filter.json")["Configuracion General"].token;
 
 const shards = new ShardingManager("./source/main.js", {
     token: config,
