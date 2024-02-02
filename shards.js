dd100c8bd47630e13c5cc2f90928824f411f7afb
zumber <84306702+zumberr@users.noreply.github.com>
2024-02-01 22:29:41 -0500

    Update shards.js

--- shards.js
+++ shards.js
@@ -1,5 +1,16 @@
 const {ShardingManager} = require("discord.js");
-const config = require("./config.json")
+const config = require("./inhibitors/filter.json")["Configuracion General"].token;
+
+const shards = new ShardingManager("./source/main.js", {
+    token: config.token,
+    totalShards: "auto"
+});
+
+shards.on("shardCreate", shard => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] shard lanzada #${shard.id}`);
+});
+
+shards.spawn(shards.totalShards, 10000);
 
 const shards = new ShardingManager("./source/main.js", {
     token: config.token,
