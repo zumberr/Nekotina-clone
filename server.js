fd091a325463f67f2b440693f08ed83717888ebc
zumber <84306702+zumberr@users.noreply.github.com>
2024-03-22 10:29:37 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+/*const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+  console.log(Date.now() + ' Ping Received');
+  response.sendStatus(200);
+});*/
